426610ea1eafe0dd398d071811927c7b0a702113
otgon123 <220054eo@yse-c.net>
2023-10-18 09:59:36 +0900

    add main

--- express_sample2/router.js
+++ express_sample2/router.js
@@ -1,7 +1,52 @@
-// express module
+// expressモジュール読み込み
 const express = require('express')
-// router object sakusei
+// Routerオブジェクトを生成
 const router = express.Router()
 
-//module
+// GETリクエストの処理
+router.get('/', (req, res) => {
+    // リクエストの処理
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    // レスポンスの処理
+    res.send('Hello!!!!!!')
+})
+
+router.get('/profile', (req, res) => {
+    res.send('プロフィール')
+})
+
+
+// /item/xxx のルーティング（パスパラメーター）
+router.get('/item/:id', (req, res) => {
+   const id = req.params.id 
+
+   res.send(id)
+})
+
+// POSTリクエスト
+router.post('/auth', (req, res) => {
+    // POSTデータ取得
+    var loginName = req.body.login_name
+    var password = req.body.password
+    console.log(loginName, password)
+
+    var message = "ログイン失敗"
+    // .env で設定した値で、ログインチェック
+    // TODO：データベースに接続してユーザ取得
+    // TODO：パスワードはハッシュ値でチェック
+    if (loginName == process.env.LOGIN_NAME
+        && password == process.env.PASSWORD) {
+            message = "ログイン成功"
+        //TODO ログインが成功したらユーザの状態を保存
+        //TODO ログイン後のページの転送
+    } else {
+        // TODO ログイン画面に戻す
+    }
+    res.send(message)
+})
+
+// モジュール化
 module.exports = router
