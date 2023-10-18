528b8413dfa6e6d257a7b5966f86d736a1d8df53
otgon123 <220054eo@yse-c.net>
2023-10-18 11:00:01 +0900

    ejs install

--- express_sample2/server.js
+++ express_sample2/server.js
@@ -2,7 +2,8 @@
 const express = require('express')
 // dotenvモジュール読み込み
 const dotenv = require('dotenv')
-const router = require('./router')
+// routerモジュール読み込み
+const routes = require('./routes')
 
 // dotenvの設定読み込み
 dotenv.config()
@@ -12,51 +13,18 @@ const PORT = process.env.PORT
 // サーバ作成
 const app = express()
 
-// middleware setting
+// ミドルウェアの設定
 // publicフォルダを静的コンテンツのフォルダに設定
 app.use(express.static(__dirname + '/public'))
 
-// url encode
+// URLエンコード
 app.use(express.urlencoded({ extended: true }))
 
-//routing
-app.use(router)
-// get request
-app.get('/', (req, res) => {
-    // リクエストの処理
-    console.log(req.body)
-    console.log(req.url)
-    console.log(req.query)
+// EJSをテンプレートエンジンとして設定
+app.set('view engine', 'ejs')
 
-    // responce process
-    res.send('Hello!!!!!!')
-})
-
-app.get('/profile', (req, res) => {
-    res.send('profile page')
-})
-
-// post request
-app.post('/auth', (req, res) => {
-    // POSTデータ取得
-    var loginName = req.body.login_name
-    var password = req.body.password
-    console.log(loginName, password)
-
-    var message = "login failed"
-    // .env で設定した値で、ログインチェック
-    // TODO：データベースに接続してユーザ取得
-    // TODO：パスワードはハッシュ値でチェック
-    if (loginName == process.env.LOGIN_NAME
-        && password == process.env.PASSWORD) {
-            message = "login success"
-        //TODO ログインが成功したらユーザの状態を保存
-        //TODO ログイン後のページの転送
-    } else {
-        // TODO ログイン画面に戻す
-    }
-    res.send(message)
-})
+// ルーティングを有効
+app.use(routes)
 
 //　サーバ停止: 起動中のターミナルで Ctrl + C
 // サーバ待機（Listen）
